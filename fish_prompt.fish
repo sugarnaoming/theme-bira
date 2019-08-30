58a960d8ff694537ac3526c1913dae3026eaf70b
Naoaki Sato <naoaksat@yahoo-corp.jp>
2019-08-30 14:25:37 +0900

    fix k8s status

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -43,20 +43,20 @@ function __kubernetes_status
   set -l config $KUBECONFIG
   [ -z "$config" ]; and set -l config "$HOME/.kube/config"
   if [ ! -f $config ]
-    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no config"
+    echo -n (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no config"
     return
   end
 
   set -l ctx (kubectl config current-context 2>/dev/null)
   if [ $status -ne 0 ]
-    echo (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no context"
+    echo -n (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no context"
     return
   end
 
   set -l ns (kubectl config view -o "jsonpath={.contexts[?(@.name==\"$context\")].context.namespace}")
   [ -z $ns ]; and set -l ns 'default'
 
-  echo (set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color white)"($ctx$KUBECTL_PROMPT_SEPARATOR$ns)"
+  echo -n (set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color white)"($ctx$KUBECTL_PROMPT_SEPARATOR$ns)"
 end
 
 function __ruby_version
