727b322e91a7e8e893872ac498be3a0812a45b1b
Naoaki Sato <naoaksat@yahoo-corp.jp>
2019-08-30 16:26:04 +0900

    fix k8s status

--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,6 +1,9 @@
 # Theme based on Bira theme from oh-my-zsh: https://github.com/robbyrussell/oh-my-zsh/blob/master/themes/bira.zsh-theme
 # Some code stolen from oh-my-fish clearance theme: https://github.com/bpinto/oh-my-fish/blob/master/themes/clearance/
 
+# Theme based on Bira theme from oh-my-zsh: https://github.com/robbyrussell/oh-my-zsh/blob/master/themes/bira.zsh-theme
+# Some code stolen from oh-my-fish clearance theme: https://github.com/bpinto/oh-my-fish/blob/master/themes/clearance/
+
 function __user_host
   set -l content 
   if [ (id -u) = "0" ];
@@ -37,25 +40,56 @@ function __git_status
   end
 end
 
-function __kubernetes_status
-  [ -z "$KUBECTL_PROMPT_ICON" ]; and set -l KUBECTL_PROMPT_ICON "⎈"
-  [ -z "$KUBECTL_PROMPT_SEPARATOR" ]; and set -l KUBECTL_PROMPT_SEPARATOR "/"
-  [ -z "$config" ]; and set -l config "$HOME/.kube/config"
-  if [ ! -f $config ]
-    echo -n (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no config"
+function __kube_ps_update_cache
+  function __kube_ps_cache_context
+    set -l ctx (kubectl config current-context 2>/dev/null)
+    if /bin/test $status -eq 0
+      set -g __kube_ps_context "$ctx"
+    else
+      set -g __kube_ps_context "n/a"
+    end
+  end
+
+  function __kube_ps_cache_namespace
+    set -l ns (kubectl config view --minify -o 'jsonpath={..namespace}' 2>/dev/null)
+    if /bin/test -n "$ns"
+      set -g __kube_ps_namespace "$ns"
+    else
+      set -g __kube_ps_namespace "default"
+    end
+  end
+
+  set -l kubeconfig "$KUBECONFIG"
+  if /bin/test -z "$kubeconfig"
+    set kubeconfig "$HOME/.kube/config"
+  end
+
+  if /bin/test "$kubeconfig" != "$__kube_ps_kubeconfig"
+    __kube_ps_cache_context
+    __kube_ps_cache_namespace
+    set -g __kube_ps_kubeconfig "$kubeconfig"
+    set -g __kube_ps_timestamp (date +%s)
     return
   end
 
-  set -l cc (kubectl config get-contexts | grep \* 2>/dev/null)
-  kubectl config get-contexts | grep \* | awk '{print $2, $5}' | read ctx ns
-  if [ -z $ctx ]
-    echo -n (set_color red)$KUBECTL_PROMPT_ICON" "(set_color white)"no context"
-    return
+  for conf in (string split ':' "$kubeconfig")
+    if /bin/test -r "$conf"
+      if /bin/test -z "$__kube_ps_timestamp"; or /bin/test (/usr/bin/stat -f '%m' "$conf") -gt "$__kube_ps_timestamp"
+        __kube_ps_cache_context
+        __kube_ps_cache_namespace
+        set -g __kube_ps_kubeconfig "$kubeconfig"
+        set -g __kube_ps_timestamp (date +%s)
+        return
+      end
+    end
   end
-
-  [ -z $ns ]; and set -l ns 'default'
-  echo -n (set_color cyan)$KUBECTL_PROMPT_ICON" "(set_color white)"($ctx$KUBECTL_PROMPT_SEPARATOR$ns)"
 end
+
+function __kube_prompt
+  __kube_ps_update_cache
+  echo -n (set_color blue)"⎈ "(set_color normal)"$__kube_ps_context/$__kube_ps_namespace"
+end
+
 
 function __ruby_version
   if type "rvm-prompt" > /dev/null 2>&1
@@ -73,9 +107,9 @@ function fish_prompt
   echo -n (set_color white)"╭─"(set_color normal)
   __user_host
   __current_path
-  __ruby_version
+#  __ruby_version
   __git_status
-  __kubernetes_status
+  __kube_prompt
   echo -e ''
   echo (set_color white)"╰─"(set_color --bold white)"\$ "(set_color normal)
 end
